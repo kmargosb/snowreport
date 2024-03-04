f89755c5b15cea96af0d9a44e59d755cdb9b5947
Nelson Camargo <cmargosb@gmail.com>
2024-03-04 19:39:04 +0100

    eviroment config google api key

--- src/components/stationcomponent/Weather.jsx
+++ src/components/stationcomponent/Weather.jsx
@@ -1,5 +1,6 @@
 'use client'
-
+import dotenv from 'dotenv';
+dotenv.config();
 import { useState, useEffect } from 'react'
 import Loading from '../loading/Loading'
 import MediumWidget from '../widgetsWeather/MediumWidget';
@@ -17,7 +18,7 @@ const Weather = ({ stationDB }) => {
         const address = stationDB?.direccion
         const googleMapsApiKey = "AIzaSyAzKp5StTGvc7_kydK2MoE_kXhHj2GTDWc"
         const urlGoogleGeo = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${googleMapsApiKey}`
-        
+
         // Fetch para las position
         const response = await fetch(urlGoogleGeo);
         const data = await response.json();
@@ -34,7 +35,7 @@ const Weather = ({ stationDB }) => {
   useEffect(() => {
     if (!position) return;
 
-    const apiKey = "aefa98ee63403acca6298ea6a8e43e35"
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY
     const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${position.lat}&lon=${position.lng}&appid=${apiKey}&lang=sp`;
     const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.lat}&lon=${position.lng}&appid=${apiKey}`;
 
